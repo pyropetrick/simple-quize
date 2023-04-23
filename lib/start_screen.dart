427bb02bb45c083506556ab1366a7cda2c53008d
Pavel <pavel.chernenko97@gmail.com>
2023-04-23 13:33:39 +0300

    feat: add question and start screens

---
+++ lib/start_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 200,
+            height: 200,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            "Learn Flutter the fun anyway",
+            style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.w300, color: Colors.white),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: const Color.fromARGB(255, 230, 213, 242),
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text(
+              "Start Quiz",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
