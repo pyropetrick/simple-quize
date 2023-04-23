7e649b6ecc4020290ea6819538d82698ba02f986
Pavel <pavel.chernenko97@gmail.com>
2023-04-23 13:34:35 +0300

    refactor: main.dart and migrate all logic to quiz class

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quize_project/ui/gradient_container.dart';
+
+import 'quiz.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  static const colors = [
-    Color.fromARGB(255, 46, 4, 118),
-    Color.fromARGB(255, 21, 5, 142)
-  ];
-  @override
-  MaterialApp build(context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: GradientContainer(colors)),
-      ),
-    );
-  }
+  runApp(const Quiz());
 }
